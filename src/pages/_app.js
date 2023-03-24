ab9dc198958bac63478d53e03f9b8c64c2a9efce
Suraj Pandey <shpcore103@gmail.com>
2023-03-24 10:02:36 +0530

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,33 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import * as React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "@/utils/theme";
+import createEmotionCache from "@/utils/createEmotionCache";
+import { StyledEngineProvider } from "@mui/material/styles";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache({
+  key: "css",
+  prepend: true,
+});
+
+export default function App(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </CacheProvider>
+  );
 }
